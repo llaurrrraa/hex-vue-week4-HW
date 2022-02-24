ed77b6c97da22872e15fdeed8e52e2165f0796ed
llaurrrraa <ha886275@gmail.com>
2022-02-24 15:45:48 +0800

    update pagination

--- javascript/pagination.js
+++ javascript/pagination.js
@@ -3,8 +3,8 @@ export default {
     props:['pages'],
     template:`<nav aria-label="Page navigation example">
     <ul class="pagination">
-      <li class="page-item" :class="{ disabled : !pages.has_pre }" >
-        <a class="page-link" href="#" aria-label="Previous">
+      <li class="page-item" :class="{ disabled : pages.current_page === 1 }" >
+        <a class="page-link" href="#" aria-label="Previous" @click="$emit('get-products',pages.current_page - 1)">
           <span aria-hidden="true">&laquo;</span>
         </a>
       </li>
@@ -13,7 +13,7 @@ export default {
         <a class="page-link" href="#" @click="$emit('get-products',page)">{{page}}</a>
       </li>
       <li class="page-item" :class="{ disabled : !pages.has_next }">
-        <a class="page-link" href="#" aria-label="Next" >
+        <a class="page-link" href="#" aria-label="Next"  @click="$emit('get-products',pages.current_page + 1)">
           <span aria-hidden="true">&raquo;</span>
         </a>
       </li>
